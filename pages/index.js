f9bd2344483f83548df66a98f58283c5c431f7a2
Santosh Devnath <santoshdevnath15@gmail.com>
2022-08-08 10:46:47 +0530

    seo for home page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,38 @@
+import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
 import { getAllPostsForHome } from "../services/api";
 
 export default function Home({ allPosts: { edges } }) {
   return (
-    <div className="container mx-auto lg:px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="lg:col-span-8 col-span-1">
-          {edges.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-          {/* <div className="lg:sticky relative top-8">
+    <>
+      <Head>
+        <title data-rh="true">
+          BabaCric | Latest Sports News , Dream11 Prediction and Tips
+        </title>
+        <meta
+          name="description"
+          content="Get cricket news, tips and analysis reports that help you to create teams on Fantasy platforms like Dream11, Vision11, Playerzpot etc "
+        />
+
+        <link rel="canonical" href="http://babacric.in/" />
+      </Head>
+      <div className="container mx-auto lg:px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="lg:col-span-8 col-span-1">
+            {edges.map((post, index) => (
+              <PostCard key={index} post={post.node} />
+            ))}
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            {/* <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div> */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
@@ -32,5 +46,6 @@ export async function getStaticProps({ preview = false }) {
       posts,
       allPosts,
     },
+    revalidate: 10, //10 minutes
   };
 }
